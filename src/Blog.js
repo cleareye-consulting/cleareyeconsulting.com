0dc6849c028431591a3581f550d0a2f2af7da6bb
Michael Gant <jmgant@cleareyeconsulting.com>
2019-04-15 09:58:20 -0400

    Changed blog links to avoid Medium paywall

--- src/Blog.js
+++ src/Blog.js
@@ -4,21 +4,21 @@ export default class Blog extends Component {
 
     posts = [
         {
-            url: 'https://medium.com/@jmgant.cleareyeconsulting/how-to-replace-your-old-as400-erp-without-wrecking-your-operations-26e442b46151',
+            url: 'https://medium.com/@jmgant.cleareyeconsulting/how-to-replace-your-old-as400-erp-without-wrecking-your-operations-26e442b46151?source=friends_link&sk=4ccb67dc822fcf14b76ada19a277056a',
             title: 'How to Replace Your Old AS400 ERP (Without Wrecking Your Operations)',
             date: new Date(2019, 3, 8),
             author: 'Michael Gant',
             summary: 'SMBs that have been operating for more than a decade or two may be struggling to innovate because their operations are completely dependent on aging AS400 ERP software. This article provides some guidance on how to begin the process of replacing those legacy systems and start moving forward again.'
         },
         {
-            url: 'https://medium.com/@jmgant.cleareyeconsulting/concerning-microsoft-access-26d3308fece0',
+            url: 'https://medium.com/@jmgant.cleareyeconsulting/concerning-microsoft-access-26d3308fece0?source=friends_link&sk=09e2c5b4c00775fea9d6d2ae271cc9c7',
             title: 'Concerning Microsoft Access',
             date: new Date(2019, 3, 5),
             author: 'Michael Gant',
             summary: 'Microsoft Access is a great tool with a variety of uses. But its issues with robustness and maintainability have been the source of many an unexpected bad day for SMBs that rely on Access applications for their day-to-day operations. This post explores when to use Access, when to look into other options, and how some of those other options compare.'
         },
         {
-            url: 'https://medium.com/@jmgant.cleareyeconsulting/what-is-business-value-148a6932e65',
+            url: 'https://medium.com/@jmgant.cleareyeconsulting/what-is-business-value-148a6932e65?source=friends_link&sk=d255283927970d494fa29a3a3352e7ae',
             title: 'What is “Business Value”',
             date: new Date(2018, 11, 8),
             author: 'Michael Gant',
