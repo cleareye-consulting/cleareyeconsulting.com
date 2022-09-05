ee5d038ec8bdb1001352353b7f96d1013632d78b
Michael Gant <jmgant@cleareyeconsulting.com>
2022-09-05 17:02:05 -0400

    Simplify and clean up

--- src/Layout.js
+++ src/Layout.js
@@ -1,82 +1,106 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from './assets/images/favicon.svg';
-import mailIcon from './assets/images/mail.svg';
-import phoneIcon from './assets/images/phone.svg';
-import facebookIcon from './assets/images/facebook_1.svg';
-import twitterIcon from './assets/images/twitter.svg';
-import linkedInIcon from './assets/images/linkedin.svg';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from './assets/images/favicon.svg'
+import mailIcon from './assets/images/mail.svg'
+import phoneIcon from './assets/images/phone.svg'
+import facebookIcon from './assets/images/facebook_1.svg'
+import twitterIcon from './assets/images/twitter.svg'
+import linkedInIcon from './assets/images/linkedin.svg'
 
 export default class Layout extends Component {
-    render() {
-        return (
-            <div className="container">
-                <header>
-                    <nav className="navbar navbar-expand-lg navbar-light bg-none">
-                        <Link className="navbar-brand mr-5" to="/" id="navbar-banner">
-                            <img src={logo} height="65" alt="ClearEye Consulting" /> ClearEye Consulting
-                        </Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item mr-3">
-                                    <Link className="nav-link text-primary" to="/services">Services</Link>
-                                </li>
-                                <li className="nav-item mr-3">
-                                    <Link className="nav-link text-primary" to="/approach">Approach</Link>
-                                </li>
-                                <li className="nav-item mr-3">
-                                    <Link className="nav-link text-primary" to="/about">About</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </header>
-                <main>
-                    <div className="container">
-                        {this.props.children}
-                    </div>
-                </main>
-                <footer className="bg-light">
-                    <section className="container">
-                        <nav className="navbar navbar-expand-lg navbar-light">
-                            <div className="navbar-collapse">
-                                <ul className="navbar-nav mr-auto">
-                                    <li className="nav-item">
-                                        <Link className="nav-item nav-link" to="/">Home</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-item nav-link" to="/privacy">Privacy</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-item nav-link" to="/terms">Terms</Link>
-                                    </li>
-                                </ul>
-                                <span className="navbar-text ml-3">
-                                    <img src={mailIcon} alt="email" />&nbsp;<a href="mailto:info@cleareyeconsulting.com">info@cleareyeconsulting.com</a>
-                                </span>
-                                <span className="navbar-text ml-3">
-                                    <img src={phoneIcon} alt="phone" />&nbsp;<a href="tel:+1-423-651-0178">423-651-0178</a>
-                                </span>
-                                <span className="navbar-text ml-3">
-                                    <a href="https://www.facebook.com/cleareyeconsulting" target="_blank" rel="noopener noreferrer"><img width="24" height="24" src={facebookIcon} alt="facebook"></img></a>
-                                </span>
-                                <span className="navbar-text ml-3">
-                                    <a href="https://twitter.com/ClearEyeCnsltg" target="_blank" rel="noopener noreferrer"><img width="24" height="24" src={twitterIcon} alt="facebook"></img></a>
-                                </span>
-                                <span className="navbar-text ml-3">
-                                    <a href="https://www.linkedin.com/company/cleareyeconsulting/" target="_blank" rel="noopener noreferrer"><img width="24" height="24" src={linkedInIcon} alt="facebook"></img></a>
-                                </span>
-                            </div>
-                        </nav>
-                    </section>
-                    <section className="bg-dark text-light text-center">
-                        Copyright &copy; 2019 ClearEye Consulting, LLC
-                    </section>
-                </footer>
-            </div >
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-light bg-none">
+            <Link className="navbar-brand mr-5" to="/" id="navbar-banner">
+              <img src={logo} height="65" alt="ClearEye Consulting" /> ClearEye Consulting
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item mr-3">
+                  <Link className="nav-link text-primary" to="/services">
+                    Services
+                  </Link>
+                </li>
+                <li className="nav-item mr-3">
+                  <Link className="nav-link text-primary" to="/approach">
+                    Approach
+                  </Link>
+                </li>
+                <li className="nav-item mr-3">
+                  <Link className="nav-link text-primary" to="/about">
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <div className="container">{this.props.children}</div>
+        </main>
+        <footer className="bg-light">
+          <section className="container">
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <div className="d-flex justify-content-left pt-2">
+                  <Link className="nav-link text-primary pe-3" to="/">
+                    Home
+                  </Link>
+                  <Link className="nav-link text-primary pe-3" to="/privacy">
+                    Privacy
+                  </Link>
+                  <Link className="nav-link text-primary" to="/terms">
+                    Terms
+                  </Link>
+                </div>
+              </div>
+              <div className="col-12 col-md-6 text-md-end my-2">
+                <span className="navbar-text me-2 me-md-0">
+                  <img src={mailIcon} alt="email" />
+                  &nbsp;<a href="mailto:info@cleareyeconsulting.com">info@cleareyeconsulting.com</a>
+                </span>
+                <span className="navbar-text ms-md-3 me-2 me-md-0">
+                  <img src={phoneIcon} alt="phone" />
+                  &nbsp;<a href="tel:+1-423-651-0178">423-651-0178</a>
+                </span>
+                <span className="navbar-text ms-md-3 me-2 me-md-0">
+                  <a href="https://www.facebook.com/cleareyeconsulting" target="_blank" rel="noopener noreferrer">
+                    <img width="24" height="24" src={facebookIcon} alt="facebook"></img>
+                  </a>
+                </span>
+                <span className="navbar-text ms-md-3 me-2 me-md-0">
+                  <a href="https://twitter.com/ClearEyeCnsltg" target="_blank" rel="noopener noreferrer">
+                    <img width="24" height="24" src={twitterIcon} alt="facebook"></img>
+                  </a>
+                </span>
+                <span className="navbar-text ms-md-3">
+                  <a href="https://www.linkedin.com/company/cleareyeconsulting/" target="_blank" rel="noopener noreferrer">
+                    <img width="24" height="24" src={linkedInIcon} alt="facebook"></img>
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="d-flex justify-content-between">
+              <div className="d-flex justify-content-start"></div>
+              <div className="d-flex justify-content-end"></div>
+            </div>
+          </section>
+          <section className="bg-dark text-light text-center">Copyright &copy; 2019 ClearEye Consulting, LLC</section>
+        </footer>
+      </div>
+    )
+  }
 }
--- src/Services.js
+++ src/Services.js
@@ -1,75 +1,61 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import globeIcon from './assets/images/globe.svg';
-import databaseIcon from './assets/images/database.svg';
-import computerIcon from './assets/images/imac.svg';
-import cloudIcon from './assets/images/cloud.svg';
-import tapeIcon from './assets/images/voicemail.svg';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import globeIcon from './assets/images/globe.svg'
+import databaseIcon from './assets/images/database.svg'
+import computerIcon from './assets/images/imac.svg'
+import cloudIcon from './assets/images/cloud.svg'
+import tapeIcon from './assets/images/voicemail.svg'
 
 export default class Services extends Component {
-    render() {
-        return (
-            <React.Fragment>                
-                <h2>What We Do</h2>
-                <p>ClearEye Consulting is a software development and business technology consulting firm serving small and medium-sized businesses. What, exactly, do we mean by that?</p>
-                <h4>Business Technology Consulting</h4>
-                <p>Let's be honest. Depending on exactly what line of business you're in, there's a pretty good chance you don't need custom software. At least not <em>totally</em> custom software. There are products to meet nearly every conceivable need. Do we know about everything that's out there? Of course not! But we understand how business technology works from the inside out. We can help you evaluate your current situation, determine what you really need, and then help you find and begin using it.</p>
-                <h4>Software Development</h4>
-                <p>On the other hand, maybe there's not an existing solution that exactly meets your needs. Maybe your company has a highly specialized workflow or you have to work with a special set of data. No problem! We love building things!</p>
-                <p>Here are some of the types of things we're particularly good at building:</p>
-                <div className="row">
-                    <div className="col-md-1">
-                        <img src={globeIcon} alt="Globe" />
-                    </div>
-                    <div className="col">
-                        <p className="header6">Public Web Sites and APIs</p>
-                        <p>We've been on the web since the early days. If you're looking for a graphically rich website with animations, videos, etc., well, we can probably recommend somebody. But if your goal is to connect your back-end systems to your website, or create a direct sales channel, or connect to your partners via B2B, now that's right up our alley!</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-1">
-                        <img src={computerIcon} alt="Computer" />
-                    </div>
-                    <div className="col">
-                        <p className="header6">Desktop applications</p>
-                        <p>We have many years of experience building rich client and desktop applications. For many clients, a browser-based application may be a better fit these days. But if you're heavily invested in the desktop, we can help you move forward with what you've got.</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-1">
-                        <img src={tapeIcon} alt="Tape" />
-                    </div>
-                    <div className="col">
-                        <p className="header6">Batch processing</p>
-                        <p>Batch processing is unglamorous, but it makes the world go 'round. We can help you automate your day-to-day processes, and also possibly help you modernize your existing batch runs.</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-1">
-                        <img src={databaseIcon} alt="Database" />
-                    </div>
-                    <div className="col">
-                        <p className="header6">Database Design and Development</p>
-                        <p>We've worked with SQL Server since the very early days. Also Sybase (now SAP) ASE, DB2/UDB, and MySQL. We've also worked with MongoDB and other NoSQL options. If you need help with your database, chances are we can provide it.</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-1">
-                        <img src={cloudIcon} alt="Cloud" />
-                    </div>
-                    <div className="col">
-                        <p className="header6">Cloud computing</p>
-                        <p>We've worked with IAAS, PAAS, and SAAS options on AWS, Azure, and Google Cloud. We also know Dropbox, OneDrive, Google Drive, iCloud, Office 365, etc. Cloud isn't the right solution for every problem, but it's easier and much more cost-effective than many businesses realize. We can help you find the right mix of cloud, on-premise, and hybrid solutions.</p>
-                    </div>
-                </div>
-                <h4>Specialty Services</h4>
-                <p><strong>MS Access Upgrades. </strong>We don't really use Microsoft Access to build new things, or necessarily recommend its use (see our <Link to="/content/migration">migration FAQs</Link> or <a href="https://medium.com/@jmgant.cleareyeconsulting/concerning-microsoft-access-26d3308fece0?source=friends_link&sk=09e2c5b4c00775fea9d6d2ae271cc9c7" target="_blank" rel="noopener noreferrer">this article</a> for a deeper dive). But we have a lot of experience helping small business and organizations evaluate their existing Access investments and plot a course toward a more permanent solution. If you're reaching the limits of Access, give us a call!</p>
-                <p><strong>Legacy System Updates. </strong>If your small to medium-sized business has been in operation for a decade or two, it's very possible that you've got an old AS400 or System i box that's running your core operational software (often called ERP for "Enterprise Resource Planning"). If that box in the corner is limiting your growth, we know <a href="https://medium.com/@jmgant.cleareyeconsulting/how-to-replace-your-old-as400-erp-without-wrecking-your-operations-26e442b46151?source=friends_link&sk=4ccb67dc822fcf14b76ada19a277056a" target="_blank" rel="noopener noreferrer">a thing or two</a> about helping you update.</p>
-                <p><strong>Quality Control Systems. </strong>If your business involves making something, likely you've given a lot of thought to how to make sure you're making it correctly (here's some <a href="https://medium.com/@jmgant.cleareyeconsulting/does-your-pdca-include-technology-809b3f0c8469?source=friends_link&sk=a06b61428560b8bdad85c36c40a9991c" target="_blank" rel="noopener noreferrer">food for thought</a> along those lines). You probably even have some kind of a system for tracking your quality metrics. If you don't, or if it's not working well, we can help. We helped a first-tier supplier to several major automotive OEMs streamline and improve their quality software (case study <Link to="/content/case-studies/NameWithheld">here</Link>), and we can help you as well!</p>
-                <p>We've also recently released a beta version of our new open-source quality management application, QMEssentials. <Link to="/content/qmessentials">Check it out!</Link></p>
-
-
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <>
+        <h2>What We Do</h2>
+        <p>
+          ClearEye Consulting is a software development and business technology consulting firm serving small and medium-sized businesses.
+        </p>
+        <h4>Custom Software</h4>
+        <p>
+          We are experienced building, maintaining, and fixing software across a broad spectrum of industries with a wide variety of
+          development tools. Types of applications we often work on include:
+        </p>
+        <ul>
+          <li>Line-of-business applications</li>
+          <li>B2B and real-time API integrations</li>
+          <li>Batch and event-driven system integrations, including EDI</li>
+          <li>Database design, development, and optimization</li>
+          <li>Cloud applications</li>
+        </ul>
+        <h4>Specialty Services</h4>
+        <p>
+          <strong>MS Access Upgrades. </strong>We don't really use Microsoft Access to build new things, or necessarily recommend its use
+          (see our <Link to="/content/migration">migration FAQs</Link> or{' '}
+          <a
+            href="https://medium.com/@jmgant.cleareyeconsulting/concerning-microsoft-access-26d3308fece0?source=friends_link&sk=09e2c5b4c00775fea9d6d2ae271cc9c7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            this article
+          </a>{' '}
+          for a deeper dive). But we have a lot of experience helping small business and organizations evaluate their existing Access
+          investments and plot a course toward a more permanent solution. If you're reaching the limits of Access, give us a call!
+        </p>
+        <p>
+          <strong>Legacy System Updates. </strong>If your small to medium-sized business has been in operation for a decade or two, it's
+          very possible that you've got an old AS400 or System i box that's running your core operational software (often called ERP for
+          "Enterprise Resource Planning"). Even if a full-scale replacement isn't realistic for you right now (it's usually a bad idea
+          anyway), we can help you develop integrated hybrid solutions that will position you for future growth. If you're in this
+          situation, you may find&nbsp;
+          <a
+            href="https://medium.com/@jmgant.cleareyeconsulting/how-to-replace-your-old-as400-erp-without-wrecking-your-operations-26e442b46151?source=friends_link&sk=4ccb67dc822fcf14b76ada19a277056a"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            this article
+          </a>
+          &nbsp; helpful.
+        </p>
+      </>
+    )
+  }
 }
