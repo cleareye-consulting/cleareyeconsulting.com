481895f885456f9c90ffa20136acdc8efd35015e
John Gant <johngant@MacBookPro.home>
2026-02-10 20:07:35 -0500

    Add article about business value

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,8 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: image().optional(),
+		photoCredit: z.string().optional(),
+		canonical: z.string().url().optional(),
 		audience: z.enum(['Developer', 'Business', 'General']).default('General'),
 	}),
 });
